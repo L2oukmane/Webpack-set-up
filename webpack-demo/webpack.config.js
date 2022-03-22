53e9efe60c01649366a52c688cff16badc94a18f
Lucas Shangrita <oustanilokman@gmail.com>
2022-03-22 17:35:01 +0300

    Set up project with webpack

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+   mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+   devServer: {
+    static: './dist',
+  },
+  module: {
+
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+
+    }),
+
+  ],
+};
